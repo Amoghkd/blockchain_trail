6722d9c1385636c2d112373cb345c02881ff3a91
amogh545 <amoghk136@gmail.com>
2024-07-26 15:21:41 +0530

    called the display api and the create contract api

--- backend/controllers/getfuncdetails.js
+++ backend/controllers/getfuncdetails.js
@@ -9,22 +9,24 @@ async function getTransactionHistory(req, res) {
 
   const contractName = req.body.contractName; // Assuming the contract name is passed in the request body
   console.log(contractName) ;
+  
   try {
        const contractDetails = await getContractDetails(contractName);
-       //console.log(contractDetails);
+       console.log(contractDetails);
        if (!contractDetails) {
           return res.status(404).send({ message: "Contract not found" });
         }
         const { abi, contract_address } = contractDetails;
-        //console.log('the abi and contract add is ' ,abi,contract_address);
-
+        
     // Create a contract instance
         const contract = new ethers.Contract(contract_address, abi, provider);
-            //console.log('the contract is',contract);
+            console.log('the contract is',contract);
 
     const events = await contract.queryFilter("*", 0, "latest");
-    console.log(events);
-    
+    console.log('the event is ',events[0].args);
+    const events1=JSON.stringify(events);
+    res.status(200).send(events1);
+    //res.json(events1);
 
 } catch (error) {
     console.error("Error fetching events:", error);
